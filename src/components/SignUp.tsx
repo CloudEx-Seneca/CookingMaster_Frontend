2bbf50461e000d7e96abf064db8d3eae3768bdcb
jgibson18seneca <josephgibson@josephs-mbp.home>
2025-02-14 16:39:01 -0500

    Updated Sign Up page by adding more fields for user profile

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const SignUp: React.FC = () => {
+  const [firstname, setFirstName] = useState('');
+  const [lastname, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,6 +16,24 @@ const SignUp: React.FC = () => {
     <div>
       <h2>Sign Up</h2>
       <form onSubmit={handleSignUp}>
+      <div>
+          <label>First Name:</label>
+          <input
+            type="firstname"
+            value={firstname}
+            onChange={(e) => setFirstName(e.target.value)}
+            // required
+          />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input
+            type="lastname"
+            value={lastname}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+        </div>
         <div>
           <label>Email:</label>
           <input
@@ -29,6 +49,15 @@ const SignUp: React.FC = () => {
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // How to create check for confirm password
             required
           />
         </div>
