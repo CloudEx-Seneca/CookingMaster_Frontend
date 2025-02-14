baa8fa3d75e83827f155c75177f4f7cef01c6c0c
jgibson18seneca <josephgibson@josephs-mbp.home>
2025-02-14 16:37:12 -0500

    Created Profile Page (Placeholder)

---
+++ src/components/Profile.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+// import React from 'react';
+// Needs to show profile page with User Name and Written/Saved/Liked recipes
+
+const Profile: React.FC = () => {
+    return(
+      <div>
+        <h2>Profile Page</h2>
+        <p>User's info general info and recipes liked, saved and created (Testing phase).</p>
+      </div>
+    );
+};
+
+export default Profile;
