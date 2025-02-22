10dc2abd0f4dccb4c85f6ac702e70e929891dd21
Aldo Alvarado <odla12345@yahoo.com>
2025-02-21 20:26:14 -0500

    Added log for debugging

--- src/helpers/GetApiBaseUrl.tsx
+++ src/helpers/GetApiBaseUrl.tsx
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 
 // Fetch API environment URL from environment variable
-const API_ENV = process.env.BACKEND_API_ENV;
+const API_ENV = process.env.REACT_APP_API_URL;
+console.log(API_ENV)
 
 // Fallback to local API URL if the environment variable is not set
 const LOCAL_API = 'http://localhost:8888'; 
