c54f751615035f161d27a09eceaa72a5cfd1f172
Aldo Alvarado <odla12345@yahoo.com>
2025-02-25 14:56:10 -0500

    Added missing semicolon

--- src/helpers/GetApiBaseUrl.tsx
+++ src/helpers/GetApiBaseUrl.tsx
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 // Fetch API environment URL from environment variable
 const API_ENV = process.env.REACT_APP_API_URL;
-console.log(API_ENV)
+console.log(API_ENV);
 
 // Fallback to local API URL if the environment variable is not set
 const LOCAL_API = 'http://localhost:8888'; 
