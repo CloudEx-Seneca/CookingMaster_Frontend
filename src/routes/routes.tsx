d57ccbcb6bf9e48e55a54c2141a49461ee0d5a5e
jgibson18seneca <joseph_1.01@hotmail.com>
2025-03-07 17:02:31 -0500

    Updated Routes to add Email verification page (/verify)

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -12,6 +12,7 @@ import PasswordReset from '../components/PasswordReset.tsx';
 import ShoppingList from '../components/ShoppingList.tsx';
 import Profile from '../components/Profile.tsx';
 import RecipeDetail from '../components/RecipeDetail.tsx';
+import EmailVerified from '../components/EmailVerified.tsx';
 
 export const withNavRoutes: JSX.Element[] = [
     <Route element={<WithNav />}>
@@ -21,7 +22,7 @@ export const withNavRoutes: JSX.Element[] = [
         <Route path="/reset-password" element={<PasswordReset />} key="reset-password" />,
         <Route path="/shoppinglist" element={<ShoppingList />} key="shoppinglist" />,
         <Route path="/profile" element={<Profile />} key="profile" />
-        {/* <Route path="/emailverified" element={<EmailVerified />} key="emailverified" />         */}
+       
     </Route>
 ];
 
@@ -30,5 +31,6 @@ export const withoutNavRoutes: JSX.Element[] = [
         <Route path="/login" element={<Login />} key="login" />,
         <Route path="/" element={<Login />} key="login-alt" />,
         <Route path="/signup" element={<SignUp />} key="signup" />
+        <Route path="/verify" element={<EmailVerified />} /> 
     </Route>
 ];
