2dc28a668e4fc3fe4017e1d0148f1cf7bbff7f18
Aldo Alvarado <odla12345@yahoo.com>
2025-03-19 13:47:04 -0400

    Updated types for recipe and ingredients

--- src/types/Recipe.tsx
+++ src/types/Recipe.tsx
@@ -1,8 +1,14 @@
+export interface Ingredient {
+  id: number;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
 
 export interface Recipe {
   id: number;
   name: string;
-  ingredients: string[];
+  ingredients: Ingredient[];  // Now using the Ingredient type for ingredients
   description: string;
   image: string;
   author: string;
